b0d88da31d12327a90ea1db3952531ebfacfb7d2
Abir Al Mahmud <abir.mahmud@webbfontaine.com>
2024-11-07 12:53:58 +0600

    Add input parameter to HelloIntelliJ program

--- src/HelloIntelliJ.java
+++ src/HelloIntelliJ.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class HelloIntelliJ {
-
-
     public static void main(String[] args) {
-        System.out.println("Hello, IntelliJ!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a message to display:");
+        String message = scanner.nextLine();
+        // Display the message
+        System.out.println("You entered: " + message);
+        // Close the scanner
+        scanner.close();
     }
 }
